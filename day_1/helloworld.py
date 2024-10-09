fd1004d5ba3d2185140f60a170763f2d8eeadd17
Elian Gavilanes <elian_202@hotmail.com>
2024-10-08 20:52:39 -0500

    commit Initial

---
+++ day_1/helloworld.py
@@ -0,0 +1,27 @@
+#Exercise 1
+
+
+#Exercise 2
+print(3+4)
+print(3-4)
+print(3*4)
+print(3/4)
+print(3%4)
+print(3//4)
+print(3**4)
+
+#Exercise 3
+print('Elian')
+print('Gavilanes')
+print('Ecuador')
+print('I am enjoying 30 days of python')
+
+#Exercise 4
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type('Elian'))
+print(type('Gavilanes'))
+print(type('Ecuador'))
